a688fa8f8e781d6951e5a2e488b67e4de4501fee
Nyambayr <nymkanyambayar99@gmail.com>
2023-04-10 23:18:58 +0800

    initiliar

---
+++ src/main/java/com/example/TodoLIst/TodoLIstApplication.java
@@ -0,0 +1,13 @@
+package com.example.TodoLIst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoLIstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoLIstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TodoLIst/TodoLIstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TodoLIst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoLIstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
